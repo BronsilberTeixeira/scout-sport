d4ca2ce08428726336ed4c3c4dbd727f0ea68673
bron.teixeira.s@gmail.com <79947948+bron9782@users.noreply.github.com>
2021-12-06 17:10:30 -0300

    Posts

--- src/app/modules/posts/pages/listar-posts/listar-posts.component.ts
+++ src/app/modules/posts/pages/listar-posts/listar-posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-posts',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  criarPost(){
+    this.router.navigate(['criarPosts']);
+  }
 }
